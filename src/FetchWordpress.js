c6145ee633e7cfaee82094429108646bb20dc21e
Adrien Lapasset <aflapasset@gmail.com>
2020-02-05 12:09:20 +0100

    add dynamic articles component

--- src/FetchWordpress.js
+++ src/FetchWordpress.js
@@ -1,33 +1,55 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+import Article from "./templates/Article"
 
 export class FetchWordpress extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      posts: []
+      posts: [],
+      selectedPostId: 241
     }
   }
 
   componentDidMount() {
-    fetch('https://woke.fr/wp-json/wp/v2/posts/?')
+    fetch('https://woke.fr/wp-json/wp/v2/posts/?per_page=100')
       .then(response => response.json())
       .then(data => {
-        console.log(data)
+        // console.log(data)
         this.setState({ posts: data })
       });
   }
 
+  onClickLink(postId) {
+    this.setState({ selectedPostId: postId })
+  }
+
   render() {
-    const posts = this.state.posts.map(post =>
-      <li key={post.title.rendered}>
-        {post.title.rendered}
+    const postTitle = this.state.posts.map(post =>
+      <li key={post.id}>
+        {post.id}
+        <Link to={post.slug} onClick={() => this.onClickLink(post.id)}>{post.title.rendered}</Link>
       </li>
     )
-    console.log(posts)
+    const postContent = this.state.posts.map(post =>
+      <div key={post.id} dangerouslySetInnerHTML={{ __html: post.content.rendered }}></div>
+    )
+
     return (
-      <div style={{ backgroundColor: 'black', height: '100vh', color: "white" }}>
-        {posts[0]}
-      </div>
+      <Router>
+        <div>
+          {postTitle}
+        </div>
+        <Switch>
+          <Route path="/:slug" children={<Article id={this.state.selectedPostId} />} />
+        </Switch>
+      </Router>
     );
   }
 }
