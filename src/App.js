05f20947a23ed3b72ff91a2b3f250bb0ab9cbf30
Adrien Lapasset <aflapasset@gmail.com>
2020-02-18 12:39:49 +0100

    hotfix homepage name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import styled, { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 import GlobalStyle from 'styles/globalStyle'
 
-import Header from 'components/Header/Header.container'
+import Header from 'components/header/Header.container'
 import Home from 'components/Home/Home.container'
 import Blog from 'components/Blog/Blog.container'
 import About from 'components/About/About'
